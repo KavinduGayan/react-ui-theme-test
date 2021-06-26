e44c031732c070230e8d5f35fcc6135edddc60f8
kavindug <kavindug@hsenidmobile.com>
2021-06-26 15:16:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
+if (process.env.REACT_APP_NAME === 'client-1') {
+  require("./resource/env/client1/env.scss");
+}
+else {
+  require("./resource/env/client2/env.scss");
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{process.env.REACT_APP_NAME}</h1>
     </div>
   );
 }
